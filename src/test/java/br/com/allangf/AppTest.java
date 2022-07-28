40d6b3b6174dfb663d5ab8badccc056a3ec5c3ae
allan201gf <allan201gf@gmail.com>
2022-07-28 18:56:05 -0300

    Initial commit

---
+++ src/test/java/br/com/allangf/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.allangf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
